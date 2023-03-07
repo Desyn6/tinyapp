d54eba00b5103109d773eeec9596904481265c36
Denny <Dennyng1989@hotmail.com>
2023-03-06 21:26:50 -0800

    Implement example /hello route to demonstrate .get method

--- express_server.js
+++ express_server.js
@@ -17,6 +17,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// example route for demonstrating express.get method
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
